82fb96047384c60cb3f75952ac8c265628a9f14c
nberkouk <47812924+nberkouk@users.noreply.github.com>
2022-02-22 17:24:54 +0100

    add adversarial benchmark notebook

--- gdeep/topology_layers/__init__.py
+++ gdeep/topology_layers/__init__.py
@@ -1,10 +1,7 @@
-from .persformer import Persformer,\
-    GraphClassifier, DeepSet, PytorchTransformer
-from .modules import ISAB, PMA, SAB, FastAttention
-from .preprocessing import load_data, load_augmented_data_as_tensor,\
-    load_data_as_tensor, pad_pds, balance_binary_dataset, print_class_balance
 
-from .attention_modules import AttentionLayer, InducedAttention, AttentionPooling
+from .persformer import Persformer,\
+    GraphClassifier
+from .modules import ISAB, PMA, SAB, FastAttention
 
 __all__ = [
     'Persformer',
@@ -12,17 +9,5 @@ __all__ = [
     'ISAB',
     'PMA',
     'SAB',
-    'FastAttention',
-    'load_data',
-    'load_augmented_data_as_tensor',
-    'load_data_as_tensor',
-    'pad_pds',
-    'AttentionLayer',
-    'InducedAttention',
-    'AttentionPooling',
-    'Persformer',
-    'DeepSet',
-    'PytorchTransformer',
-    'balance_binary_dataset',
-    'print_class_balance',
-    ]
+    'FastAttention'
+]
