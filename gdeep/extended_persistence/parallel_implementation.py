5857dc08eefa3b9681ad512ace33112278301715
raphaelreinauer <reinauerr@googlemail.com>
2022-03-10 14:59:19 +0100

    commented out examples

--- gdeep/extended_persistence/parallel_implementation.py
+++ gdeep/extended_persistence/parallel_implementation.py
@@ -87,47 +87,47 @@ def compute_death_times(adj_mat, filtration_vals):
 
     return (death_matrix != 0).argmax(axis=1)
 # %%
-path_to_data = join('tests', 'data')
+# path_to_data = join('tests', 'data')
 
 
-# %%
-with open(join(path_to_data, 'xlarge_filtered_graph.npy'), 'rb') as f:
-    adj_mat = np.load(f)
-    filtration_vals = np.load(f)
-del f
-np.count_nonzero(get_local_minima(adj_mat, filtration_vals))
+# # %%
+# with open(join(path_to_data, 'xlarge_filtered_graph.npy'), 'rb') as f:
+#     adj_mat = np.load(f)
+#     filtration_vals = np.load(f)
+# del f
+# np.count_nonzero(get_local_minima(adj_mat, filtration_vals))
 
-# %%
-with open(join(path_to_data, 'reddit12k_sample_graph.npy'), 'rb') as f:
-    adj_mat = np.load(f)
-del f
-filtration_vals = HeatKernelSignature(adj_mat, 0.1)()
-assert filtration_vals.shape == np.shape(adj_mat[0],), f"filtration_vals has shape {filtration_vals.shape}"
-#nx.draw(graph, labels={v: f for (v, f) in enumerate(filtration_vals.tolist())},with_labels=True)
+# # %%
+# with open(join(path_to_data, 'reddit12k_sample_graph.npy'), 'rb') as f:
+#     adj_mat = np.load(f)
+# del f
+# filtration_vals = HeatKernelSignature(adj_mat, 0.1)()
+# assert filtration_vals.shape == np.shape(adj_mat[0],), f"filtration_vals has shape {filtration_vals.shape}"
+# #nx.draw(graph, labels={v: f for (v, f) in enumerate(filtration_vals.tolist())},with_labels=True)
 
-np.count_nonzero(get_local_minima(adj_mat, filtration_vals))
+# np.count_nonzero(get_local_minima(adj_mat, filtration_vals))
 
-dgraph = nx.from_numpy_matrix(adj_mat)
-# %%
-
-
-# %%
-graph_size = 5
-adj_mat = np.zeros((graph_size, graph_size))
-adj_mat[0, 2] = adj_mat[1, 2] = adj_mat[2, 4] = adj_mat[3, 4] = 1
-adj_mat += adj_mat.T
-filtration_vals = np.array(list(range(1, graph_size + 1)))
-max_filtration = filtration_vals.max()
-graph = nx.from_numpy_matrix(adj_mat)
-nx.draw(graph, with_labels=True)
-assert np.count_nonzero(get_local_minima(adj_mat, filtration_vals)) == graph_size - 2
+# dgraph = nx.from_numpy_matrix(adj_mat)
+# # %%
 
 
-# %%
-from gdeep.extended_persistence.gudhi_implementation import graph_extended_persistence_gudhi
-%timeit graph_extended_persistence_gudhi(adj_mat, filtration_vals)
-%timeit x = compute_death_times(adj_mat, filtration_vals)
-# %%
-%timeit x = compute_death_times(adj_mat, filtration_vals)
+# # %%
+# graph_size = 5
+# adj_mat = np.zeros((graph_size, graph_size))
+# adj_mat[0, 2] = adj_mat[1, 2] = adj_mat[2, 4] = adj_mat[3, 4] = 1
+# adj_mat += adj_mat.T
+# filtration_vals = np.array(list(range(1, graph_size + 1)))
+# max_filtration = filtration_vals.max()
+# graph = nx.from_numpy_matrix(adj_mat)
+# nx.draw(graph, with_labels=True)
+# assert np.count_nonzero(get_local_minima(adj_mat, filtration_vals)) == graph_size - 2
 
-# %%
+
+# # %%
+# from gdeep.extended_persistence.gudhi_implementation import graph_extended_persistence_gudhi
+# %timeit graph_extended_persistence_gudhi(adj_mat, filtration_vals)
+# %timeit x = compute_death_times(adj_mat, filtration_vals)
+# # %%
+# %timeit x = compute_death_times(adj_mat, filtration_vals)
+
+# # %%
